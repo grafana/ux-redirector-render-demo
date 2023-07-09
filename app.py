8bfae2ec3223be677f60fcffc7ee25a7450db346
Andrej Kiripolský <4126171+AndrejKiri@users.noreply.github.com>
2023-07-09 15:35:00 +0200

    Change main to app

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def hello():
             counter.value += 1
     return redirect(url, code=302) 
 
-if __name__ == '__main__':
+if __name__ == '__app__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
